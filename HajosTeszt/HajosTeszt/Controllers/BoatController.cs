221f9c3b68c5d18e98e2b5c95a7d71bc2328dba6
barcasl3 <barcaasl3@gmail.com>
2021-04-15 12:24:54 +0200

    added controllers, html

---
+++ HajosTeszt/HajosTeszt/Controllers/BoatController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HajosTeszt.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HajosTeszt.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class BoatController : ControllerBase
+    {
+        [HttpGet]
+        [Route("questions/all")]
+        public IActionResult M1()
+        {
+            hajostesztContext context = new hajostesztContext();
+            var kerdesek = from x in context.Questions select x.QuestionText;
+
+            return new JsonResult(kerdesek);
+        }
+    }
+}
