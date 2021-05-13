f9001b0245f5d3499b311477fb4ca11f2642bd3a
barcasl3 <barcaasl3@gmail.com>
2021-05-13 12:40:37 +0200

    jokes + ignore

---
+++ HajosTeszt/HajosTeszt/Controllers/JokeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HajosTeszt.JokeModels;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HajosTeszt.Controllers
+{
+    [Route("api/jokes")]
+    [ApiController]
+    public class JokeController : ControllerBase
+    {
+        // GET: api/jokes
+        [HttpGet]
+        public IEnumerable<Joke> Get()
+        {
+            FunnyDatabaseContext context = new FunnyDatabaseContext();
+            return context.Jokes.ToList();
+        }
+
+        // GET api/jokes/5
+        [HttpGet("{id}")]
+        public Joke Get(int id)
+        {
+            FunnyDatabaseContext context = new FunnyDatabaseContext();
+
+            var joke = (from x in context.Jokes where x.JokeSk == id select x).FirstOrDefault();
+
+            return joke;
+        }
+
+        // POST api/jokes
+        [HttpPost]
+        public void Post([FromBody] Joke ujVicc)
+        {
+            FunnyDatabaseContext context = new FunnyDatabaseContext();
+            context.Jokes.Add(ujVicc);
+            context.SaveChanges();
+        }
+
+        // PUT api/<JokeController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<JokeController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            FunnyDatabaseContext context = new FunnyDatabaseContext();
+            var torlendoVicc = (from x in context.Jokes where x.JokeSk == id select x).FirstOrDefault();
+
+            context.Jokes.Remove(torlendoVicc);
+        }
+    }
+}
